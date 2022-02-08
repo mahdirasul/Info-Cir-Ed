a279a094c58a80bb40cb2a8269acf99ba9812023
mahdirasul <99278688+mahdirasul@users.noreply.github.com>
2022-02-08 20:02:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,473 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+  	<title>Computer Institute Of Rania</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="css/style.css">
+
+	</head>
+	<body>
+						<div class="col d-flex justify-content-end">
+							<div >
+				    		<p class="mb-0 d-flex">
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+				    			<a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+				    		</p>
+			        </div>
+						</div>
+				</div>
+			</div>
+		</div>
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+        <img src="images/Logo.png" width="125" height="100">	 
+             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      <div class="collapse navbar-collapse" id="ftco-nav">
+	        <ul class="navbar-nav m-auto">
+	        	
+	      	
+	         <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h5>غیابات</h5></a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+              	<a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage1\stage1.php"><h5>قۆناغی ١</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage2\stage2.php"><h5>قۆناغی ٢</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage3\stage3.php"><h5>قۆناغی ٣</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage4\stage4.php"><h5>قۆناغی ٤</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage5\stage5.php"><h5>قۆناغی ٥</h5></a>
+              </div>
+            </li>
+	        	<li class="nav-item"><a href="C:\wamp\www\website-menu-16\barnama\barnamakan.php" class="nav-link"><h5>بەرنامەکان</h5></a></li>
+	  
+			  
+			     <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h5>فێرکاری</h5></a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+              	<a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage1\ferkary1.php"><h5>قۆناغی ١</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage2\ferkary2.php"><h5>قۆناغی ٢</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage3\ferkary3.php"><h5>قۆناغی ٣</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage4\ferkary4.php"><h5>قۆناغی ٤</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage5\ferkary5.php"><h5>قۆناغی ٥</h5></a>
+              </div>
+            </li>
+			  
+			  
+			  
+		
+			  
+			   <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h5>توێژینەوەکان</h5></a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+              	<a class="dropdown-item" href="C:\wamp\www\website-menu-16\twezhenawa\web.php"><h5>Web</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\twezhenawa\pro.php"><h5>Pro</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\twezhenawa\net.php"><h5>Net</h5></a>
+              </div>
+            </li>
+	          <!-- <li class="nav-item"><a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="nav-link"><h5>وانەکان</h5></a></li> -->
+			   <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h5>وانەکان</h5></a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+              	<a class="dropdown-item" href="C:\wamp\www\\website-menu-16\stage1\wana1.php"><h5>قۆناغی ١</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage2\wana2.php"><h5>قۆناغی ٢</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage3\wana3.php"><h5>قۆناغی ٣</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage4\wana4.php"><h5>قۆناغی ٤</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\stage5\wana5.php"><h5>قۆناغی ٥</h5></a>
+              </div>
+            </li>
+	        	<!-- <li class="nav-item"><a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="nav-link"><h5>چالاکیەکان</h5></a></li> -->
+				
+				
+               <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h5>چالاکیەکان</h5></a>
+                  <!-- <ul class="dropdown">				  -->
+				   <div class="dropdown-menu" aria-labelledby="dropdown04">
+                    <a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\1.php"><h5>سیمینار </h5></a>
+                    <a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\2.php"><h5>ڤیستیڤاڵی زانستی </h5></a>
+					<a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\3.php"><h5>ڤیستیڤاڵی کلتوری </h5></a>
+					<a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\4.php"><h5>چالاکی وەرزشی </h5></a>
+					<a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\5.php"><h5>چالاکی خوێندکاران </h5></a>   
+					<a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\6.php"><h5>بۆنەکان </h5></a>
+					<a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\7.php"><h5>ژینگەیی </h5></a>
+					<a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\8.php"><h5>گەشت </h5></a>    
+					<a class="dropdown-item" href="C:\wamp\www\website-menu-16\chalake\9.php"><h5>کۆبونەوە </h5></a>
+                  </div>
+            </li>
+				
+				
+				
+	        	<!-- <li class="nav-item"><a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="nav-link"><h5>هەواڵ</h5></a></li> -->
+				   <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><h5>هەواڵ</h5></a>
+              <div class="dropdown-menu" aria-labelledby="dropdown04">
+              	<a class="dropdown-item" href="C:\wamp\www\website-menu-16\hawal\zansti.php"><h5>زانستی</h5></a>
+                <a class="dropdown-item" href="C:\wamp\www\website-menu-16\hawal\paymanga.php"><h5>پەیمانگا</h5></a>
+                <!-- <a class="dropdown-item" href="twezhenawa/net.php"><h5>Net</h5></a> -->
+              </div>
+            </li>
+			  <li class="nav-item"><a href="C:\wamp\www\website-menu-16\index\index.php#" class="nav-link"><h5>سەرەتا</h5></a></li>
+	        </ul>
+	      </div>
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
+	</section>
+	
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class=""></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1" class=""></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2" class=""></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/all.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="images/3.jpg" alt="Third slide">
+    </div>
+	<div class="carousel-item active">
+      <img class="d-block w-100" src="images/4.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<br>
+<button style="width:30cm;margin-left:3cm" type="button" class="btn btn-warning btn-lg btn-block disabled"><h3 style="color:black">نوێترین بڵاوکراوەکان</h3><h3></h3></button>
+<br>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col"><div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/5.jpg" alt="Card image cap">
+  <div class="card-body">
+  
+    <p class="card-text">سیمیناری (گەنج و ئایندەبینی)
+بڵاوکراوەی ڕۆژی::  2015-11-16
+</p>
+    <a href="content/p1.php" class="btn btn-primary">درێژە</a>
+  </div>
+</div></th>
+      <th scope="col"><div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/6.jpg" alt="Card image cap">
+  <div class="card-body">
+   
+    <p class="card-text">ڤیستیڤاڵی زانستی (٢٠١٥ - ٢٠١٦)
+بڵاوکراوەی ڕۆژی::  2016-04-21
+</p>
+    <a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="btn btn-primary">درێژە</a>
+  </div>
+</div></th>
+      <th scope="col"><div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/11.jpg" alt="Card image cap">
+  <div class="card-body">
+   
+    <p class="card-text">وێنەی دەرچوون (٢٠١٥ - ٢٠١٦)
+بڵاوکراوەی ڕۆژی::  2016-04-09
+</p>
+    <a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="btn btn-primary">درێژە</a>
+  </div>
+</div></th>
+      <th scope="col"><div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/2k.jpg" alt="Card image cap">
+  <div class="card-body">
+    
+    <p class="card-text">بەرنامەیەک بۆ دابەزاندنی ڤیدیۆ 
+بڵاوکراوەی ڕۆژی::  2015-12-25
+</p>
+    <a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="btn btn-primary">درێژە</a>
+  </div>
+</div></th>
+    </tr>
+  </thead>
+</table>
+
+<button style="width:30cm;margin-left:3cm" type="button" class="btn btn-warning btn-lg btn-block disabled"><h3 style="color:black">گەلەری پەیمانگا
+</h3><h3></h3></button>
+<br>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">
+	  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/11.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">وێنەی دەرچوون (٢٠١٥ - ٢٠١٦)</p>
+  </div>
+</div>
+</th>
+      <th scope="col"><div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/g2.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">ڤیستیڤاڵی زانستی (٢٠١٥ - ٢٠١٦)</p>
+  </div>
+</div>
+</th>
+      <th scope="col">   </th><th scope="col"><div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/g3.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">كۆبونەوەی مامۆستایان لەگەڵ خوێندکاران</p>
+  </div>
+</div></th>
+      <th scope="col">   </th><th scope="col"><div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/g4.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">سیمیناری (گەنج و ئایندەبینی)</p>
+  </div>
+</div></th>
+    </tr>
+  </thead>
+</table>
+<br>
+<button style="width:30cm;margin-left:3cm" type="button" class="btn btn-warning btn-lg btn-block disabled"><h3 style="color:black">ئاگاداریەکان</h3><h3></h3></button>
+<br>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">
+	  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/agadary.jpg" alt="Card image cap">
+  <div class="card-body">
+   <p class="card-text">
+  خشتەی تاقیکردنەوە تیۆری کۆتایی ساڵ - خولی دووەم ساڵی خوێندنی (٢٠٢٠ - ٢٠٢١)</p>
+    <a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="btn btn-primary">درێژە</a>
+  </div>
+</div>
+</th>
+      <th scope="col">
+	  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/agadary.jpg" alt="Card image cap">
+  <div class="card-body">
+  <p class="card-text">
+  ئاگاداری سەرجەم مامۆستایان و فەرمانبەران و .......
+
+</p>
+    <a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="btn btn-primary">درێژە</a>
+  </div>
+</div>
+</th>
+<th scope="col">
+	  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/agadary.jpg" alt="Card image cap">
+  <div class="card-body">
+  <p class="card-text">
+  ئەنجامی ڕیزبەندی (١٠) یەکەمەکانی قۆناغی پێنجەمی (پڕۆگرام، نێتۆڕک) 
+
+</p>
+    <a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="btn btn-primary">درێژە</a>
+  </div>
+</div>
+</th>
+<th scope="col">
+	  <div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="images/agadary.jpg" alt="Card image cap">
+  <div class="card-body">
+  <p class="card-text">
+ ئاگاداری
+پەیمانگەی کۆمپیوتەری ڕانیە سەرجەم خوێندکارانی دەرچوی پۆلی (٩)ی....
+
+</p>
+    <a href="file:///C:/Users/mahdi/Desktop/website-menu-16/index.php#" class="btn btn-primary">درێژە</a>
+  </div>
+</div>
+</th>
+    </tr>
+  </thead>
+</table>
+<br>
+<button style="width:30cm;margin-left:3cm" type="button" class="btn btn-warning btn-lg btn-block disabled"><h3 style="color:black">دەربارەی پەیمانگە
+</h3><h3></h3></button>
+<br>
+<button style="width:30cm; margin-left:3cm" type="button" class="btn btn-secondary  btn-lg btn-block">
+پەیمانگای کۆمپیوتەری ڕانیە لە ساڵی (٢٠٠٧)ەوە دامەزراوە وەکو ناوەندێکی زانستی و ئەکادیم ی حکومی کاردەکات لەپێناو بنیادنانی کۆمەڵگایەکی پێشکەوتو سەردەمی،هەروەها ئەرکی پێگەیاندن ی تاکێکی هۆشیارو بەرپرسیارە لەروی بەها و ڕەوشتی و کۆمەڵایەتیەکان و ئاشناکردنی خوێندکارە لەگەڵ پێشکە وتنەکانی جیهانی کۆمپیوتەرو تەکنەلۆژیا ئەوەش لەڕێگەی هەموو ئەو وانە جۆراو جۆرانەی لە پەیمانگاکەمان دەخوێنرێت دەمانەوێت ڕێنیشاندەرو ئاڕاستەکەری خوێنکارەکانمان بین بۆ ژیانێکی تەندروست و باشتر لە ئایندەدا هەوڵدەدەین وەگەڕ خەری توانا شاراوەکانیان بین و بەکاریان بێنین لەخزمەت خەڵکو نیشتیمان ،بەڵام پێویستە هەمیشە ئەوەمان لەبەر چاو بێت وەها کارێک کۆمەکو ئامادەیی چەند لایەنی دەوڵەت،خێزان،مامۆستا ،خوێندکار،تەواوی ستافی ئیداری پێویستە،ئەگەر نا هەمو کارو هەوڵە کانمان نیوەچڵ و نەرسکاو دەبن
+
+</button>
+<br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Footer Design</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+</head>
+<body>
+
+  <footer class="footer">
+  	 <div class="container">
+  	 	<div class="row">
+  	 		<div class="footer-col">
+  	 			<h4>چالاکیەکان</h4>
+  	 			<ul>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\1.php">سیمینارەکان</a></li>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\6.php">بۆنەکان</a></li>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\2.php">ڤیستیڤاڵی زانستی</a></li>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\3.php">ڤیستیڤاڵی کلتوری</a></li>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\4.php">چالاکی وەرزشیەکان</a></li>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\9.php">کۆبونەوەکان</a></li>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\7.php">چالاکی ژینگەیەکان</a></li>
+  	 				<li><a href="C:\wamp\www\website-menu-16\chalake\8.php">گەشتەکان</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>بەستەرەکان</h4>
+  	 			<ul>
+  	 				<li><a href="#">فێرکاریەکان</a></li>
+  	 				<li><a href="#">هەواڵی تەکنەلۆجی</a></li>
+  	 				<li><a href="#">هەواڵی پەیمانگە</a></li>
+  	 				<li><a href="#">ئاگاداریەکان</a></li>
+  	 				<li><a href="#">فێرکاریەکان</a></li>
+  	 				<li><a href="#">بەرنامەی پێویست</a></li>
+  	 				<li><a href="#">گەلەری پەیمانگا</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>پەیوەندی کردن</h4>
+  	 			<ul>
+  	 				<li><a href="#">07504991284</a></li>
+  	 				<li><a href="#">info@cir-edu.com</a></li>
+  	 			</ul>
+  	 		</div>
+  	 		<div class="footer-col">
+  	 			<h4>follow us</h4>
+  	 			<div class="social-links">
+  	 				<a href="https://www.facebook.com/Rania.computer/"><i class="fab fa-facebook-f"></i></a>
+  	 				<a href="https://account.viber.com/account/07504991284"><i class="fab fa-viber"></i></a>
+  	 			</div>
+  	 		</div>
+  	 	</div>
+  	 </div>
+  </footer>
+
+</body>
+</html>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+body{
+	line-height: 1.5;
+	font-family: 'Poppins', sans-serif;
+}
+*{
+	margin:0;
+	padding:0;
+	box-sizing: border-box;
+}
+.container{
+	max-width: 1170px;
+	margin:auto;
+}
+.row{
+	display: flex;
+	flex-wrap: wrap;
+}
+ul{
+	list-style: none;
+}
+.footer{
+	background-color: #24262b;
+    padding: 70px 0;
+}
+.footer-col{
+   width: 25%;
+   padding: 0 15px;
+}
+.footer-col h4{
+	font-size: 18px;
+	color: #ffffff;
+	text-transform: capitalize;
+	margin-bottom: 35px;
+	font-weight: 500;
+	position: relative;
+}
+.footer-col h4::before{
+	content: '';
+	position: absolute;
+	left:0;
+	bottom: -10px;
+	background-color: #e91e63;
+	height: 2px;
+	box-sizing: border-box;
+	width: 50px;
+}
+.footer-col ul li:not(:last-child){
+	margin-bottom: 10px;
+}
+.footer-col ul li a{
+	font-size: 16px;
+	text-transform: capitalize;
+	color: #ffffff;
+	text-decoration: none;
+	font-weight: 300;
+	color: #bbbbbb;
+	display: block;
+	transition: all 0.3s ease;
+}
+.footer-col ul li a:hover{
+	color: #ffffff;
+	padding-left: 8px;
+}
+.footer-col .social-links a{
+	display: inline-block;
+	height: 40px;
+	width: 40px;
+	background-color: rgba(255,255,255,0.2);
+	margin:0 10px 10px 0;
+	text-align: center;
+	line-height: 40px;
+	border-radius: 50%;
+	color: #ffffff;
+	transition: all 0.5s ease;
+}
+.footer-col .social-links a:hover{
+	color: #24262b;
+	background-color: #ffffff;
+}
+
+/*responsive*/
+@media(max-width: 767px){
+  .footer-col{
+    width: 50%;
+    margin-bottom: 30px;
+}
+}
+@media(max-width: 574px){
+  .footer-col{
+    width: 100%;
+}
+}
+
+
+
+
+
+</style>
+
+	<script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+	</body>
+</html>
+
